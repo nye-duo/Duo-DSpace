a6d91ff137d71b06b9cf07613a9a7318bf4fe124
Richard Jones <richard@cottagelabs.com>
2012-05-04 14:36:47 +0100

    Add FS compliant Entry Disseminator
    * Extend the standard sword dc entry disseminator to incorporate the FS metadata also
    * add some supporting constants
    * add a test directory, a metadata file resource, and a scratch pad (no acctual tests at this point)

--- src/main/java/no/uio/duo/DuoConstants.java
+++ src/main/java/no/uio/duo/DuoConstants.java
@@ -9,4 +9,7 @@ public class DuoConstants
     public static QName GRADE_QNAME = new QName(FS_NAMESPACE, "grade");
     public static QName EMBARGO_END_DATE_QNAME = new QName(FS_NAMESPACE, "embargoEndDate");
     public static QName EMBARGO_TYPE_QNAME = new QName(FS_NAMESPACE, "embargoType");
+
+    public static String METADATA_BUNDLE = "METADATA";
+    public static String METADATA_FILE = "metadata.xml";
 }

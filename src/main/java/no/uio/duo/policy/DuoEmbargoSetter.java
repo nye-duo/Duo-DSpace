f1d352fd795d00ce468a0c9366a12e0c761ac23d
Richard Jones <richard@cottagelabs.com>
2017-06-26 20:02:51 +0100

    update the embargosetter so it will only apply itself where required, and implement fallback to a configured alternative

--- src/main/java/no/uio/duo/policy/DuoEmbargoSetter.java
+++ src/main/java/no/uio/duo/policy/DuoEmbargoSetter.java
@@ -1,24 +1,27 @@
 package no.uio.duo.policy;
 
 import org.dspace.authorize.AuthorizeException;
-import org.dspace.content.DCDate;
-import org.dspace.content.Item;
+import org.dspace.content.*;
+import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
-import org.dspace.embargo.DefaultEmbargoSetter;
+import org.dspace.core.PluginManager;
 import org.dspace.embargo.EmbargoSetter;
+import org.dspace.handle.HandleManager;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DuoEmbargoSetter implements EmbargoSetter
 {
-    private EmbargoSetter dspaceDefault;
+    private EmbargoSetter fallback;
     private PolicyPatternManager policies;
 
     public DuoEmbargoSetter()
     {
-        this.dspaceDefault = new DefaultEmbargoSetter();
+        this.fallback = (EmbargoSetter) PluginManager.getNamedPlugin(EmbargoSetter.class, "fallback");
         this.policies = new PolicyPatternManager();
     }
 
@@ -26,20 +29,79 @@ public class DuoEmbargoSetter implements EmbargoSetter
     public DCDate parseTerms(Context context, Item item, String s)
             throws SQLException, AuthorizeException, IOException
     {
-        return this.dspaceDefault.parseTerms(context, item, s);
+        return this.fallback.parseTerms(context, item, s);
     }
 
     @Override
     public void setEmbargo(Context context, Item item)
             throws SQLException, AuthorizeException, IOException
     {
-        this.policies.applyToNewItem(item, context);
+        if (this.allow(context, item))
+        {
+            this.policies.applyToNewItem(item, context);
+        }
+        else
+        {
+            this.fallback.setEmbargo(context, item);
+        }
     }
 
     @Override
     public void checkEmbargo(Context context, Item item)
             throws SQLException, AuthorizeException, IOException
     {
-        this.policies.applyToExistingItem(item, context);
+        if (this.allow(context, item))
+        {
+            this.policies.applyToExistingItem(item, context);
+        }
+        else
+        {
+            this.fallback.checkEmbargo(context, item);
+        }
+    }
+
+    private boolean allow(Context context, Item item)
+            throws SQLException
+    {
+        String liftDateField = ConfigurationManager.getProperty("embargo.field.lift");
+        if (liftDateField == null)
+        {
+            return false;
+        }
+
+        DCValue[] dcvs = item.getMetadata(liftDateField);
+        if (dcvs.length == 0)
+        {
+            return false;
+        }
+
+        String scopeCfg = ConfigurationManager.getProperty("duo.embargo.communities");
+        if (scopeCfg == null)
+        {
+            return true;
+        }
+
+        List<Integer> communityIDs = new ArrayList<Integer>();
+        String[] handles = scopeCfg.split(",");
+        for (String handle : handles)
+        {
+            handle = handle.trim();
+            DSpaceObject dso = HandleManager.resolveToObject(context, handle);
+            if (dso instanceof Community)
+            {
+                communityIDs.add(dso.getID());
+            }
+        }
+
+        Community[] coms = item.getCommunities();
+        for (Community com : coms)
+        {
+            if (communityIDs.contains(com.getID()))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }

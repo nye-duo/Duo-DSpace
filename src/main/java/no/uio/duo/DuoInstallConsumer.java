f18c2e65145b1bbbb21410256aa516ae7e933809
Richard Jones <richard@cottagelabs.com>
2017-07-01 09:21:07 +0100

    update the existing install consumer to use the new policy pattern manager

--- src/main/java/no/uio/duo/DuoInstallConsumer.java
+++ src/main/java/no/uio/duo/DuoInstallConsumer.java
@@ -1,34 +1,19 @@
 package no.uio.duo;
 
-import org.dspace.authorize.AuthorizeException;
-import org.dspace.authorize.AuthorizeManager;
-import org.dspace.authorize.ResourcePolicy;
-import org.dspace.content.Bitstream;
-import org.dspace.content.Bundle;
-import org.dspace.content.DCDate;
+import no.uio.duo.policy.PolicyPatternManager;
 import org.dspace.content.DCValue;
-import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
 import org.dspace.core.ConfigurationManager;
-import org.dspace.core.Constants;
 import org.dspace.core.Context;
-import org.dspace.embargo.EmbargoManager;
-import org.dspace.eperson.Group;
 import org.dspace.event.Consumer;
 import org.dspace.event.Event;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 /**
  * <p>Event consumer for Duo which responds to Item Installs.</p>
  *
  * <p>This consumer will first determine if an item has embargo metadata attached to it.  If not
  * it will apply the default access policies to the item as per the Duo requirements.  To do this
- * it delegates to the DuoPolicyManager.</p>
+ * it delegates to the PolicyPatternManager.</p>
  *
  * <p><strong>Configuration</strong></p>
  *
@@ -87,57 +72,10 @@ public class DuoInstallConsumer implements Consumer
             return;
         }
 
-        // we mustn't set the policies if the item is embargoed; these will have already
-        // been removed, and should stay that way
-        if (this.isEmbargoed(context, item))
-        {
-            return;
-        }
-
-        DuoPolicyManager dpm = new DuoPolicyManager();
-        dpm.setDefaultPolicies(context, item);
-    }
-
-    private boolean isEmbargoed(Context context, Item item)
-            throws Exception
-    {
-        // if an embargo field isn't configured, then the item can't be embargoed
-        String liftDateField = ConfigurationManager.getProperty("embargo.field.lift");
-        if (liftDateField == null)
-        {
-            return false;
-        }
-
-        // if there is no embargo value, the item isn't embargoed
-        DCValue[] embargoes = item.getMetadata(liftDateField);
-        if (embargoes.length == 0)
-        {
-            return false;
-        }
-
-        // if the embargo date is in the past, then the item isn't embargoed
-        try
-        {
-            DCDate embargoDate = EmbargoManager.getEmbargoTermsAsDate(context, item);
-            if ((new Date()).getTime() > embargoDate.toDate().getTime())
-            {
-                return false;
-            }
-        }
-        catch (SQLException e)
-        {
-            throw new Exception(e);
-        }
-        catch (AuthorizeException e)
-        {
-            throw new Exception(e);
-        }
-        catch (IOException e)
-        {
-            throw new Exception(e);
-        }
-
-        return true;
+        // now simply apply the policy pattern, which will take the appropriate action
+        // depending on the state of the item at the point we pick it up
+        PolicyPatternManager ppm = new PolicyPatternManager();
+        ppm.applyToNewItem(item, context);
     }
 
     private boolean isFail(Context context, Item item)
